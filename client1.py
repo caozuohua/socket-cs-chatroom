9d30e0945cf834f1c79389997613b47be07ad6b6
caozuohua <caozuohua@qq.com>
2018-05-25 16:44:45 +0800

    整理

--- client1.py
+++ client1.py
@@ -6,41 +6,42 @@ import sys
 import socket
 from socket import *
 
-HOST = '192.168.81.88'
-PORT = 55555
-addr = (HOST, PORT)
+class ServerAddr():
+    HOST = '192.168.81.88'
+    PORT = 55555
+    BUFFERSIZE = 1024
 
-#一次接收这么多bytes
-BUFFERSIZE = 1024
-
-# SOCK_DGRAM ：UDP数据报
-s = socket(AF_INET, SOCK_DGRAM)
-# socket类s去连接服务器
-s.connect((HOST, PORT))
-
-def client_print(*msg):
-    print("CLIENT: ", msg)
-
-while True:
-    kel = input('Question >>')
-    if kel == 'exit':
-        break
-
-    client_print('input data: %s' % kel)
-    # python3 socket 只能收发二进制数据，需要转码
-    kel = kel.encode(encoding='utf-8')
-    #s.sendall(kel)
-    # 发送数据
-    try:
-        s.sendto(kel, addr)
-        # 接收数据
-        data, addr = s.recvfrom(BUFFERSIZE)
-        client_print("recived message from server({0}:{1}) : {2}".format(addr[0], addr[1], data.decode('utf-8')))
-    except ConnectionRefusedError:
-        client_print("exit client")
+def start_udp_client():
+    host = ServerAddr()
+    # SOCK_DGRAM : UDP socket 
+    s = socket(AF_INET, SOCK_DGRAM)
+    ip_port = (host.HOST, host.PORT)
+    s.connect(ip_port)
+    
+    def client_print(*msg):
+        print("CLIENT: %s" % msg)
+    
+    while True:
+        kel = input('Question >>')
+        if kel == 'exit':
+            break
+    
+        client_print('input data: %s' % kel)
+        # python3 socket 只能收发二进制数据，需要转码
+        kel = kel.encode(encoding='utf-8')
+        try:
+            # s.sendall(kel)
+            s.sendto(kel, ip_port)
+            # 接收数据
+            data, addr = s.recvfrom(host.BUFFERSIZE)
+            client_print("recived message from server({0}:{1}) : {2}".format(addr[0], addr[1], data.decode('utf-8')))
+        except ConnectionRefusedError:
+            client_print("exit client")
+            s.close()
+    
+    if not s._closed:
         s.close()
-
-if not s._closed:
-    s.close()
-
-sys.exit()
+    
+if __name__ == '__main__':
+    start_udp_client()
+    sys.exit()
--- myserver.py
+++ myserver.py
@@ -8,47 +8,49 @@ import socket
 from socket import *
 import os
 
-HOST = '192.168.81.88'
-PORT = 55555
+class ServerAddr():
+    HOST = '192.168.81.88'
+    PORT = 55555
+    BUFFERSIZE = 1024
+    logfile = 'dialog.log'
 
-#只接收这么多个bytes
-BUFFERSIZE = 1024
+def start_udp_server():
+    host = ServerAddr()
+    ip_port = (host.HOST, host.PORT)
+    # UDP socket
+    s = socket(AF_INET, SOCK_DGRAM)
+    # 重用ip和端口
+    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    s.bind(ip_port)
+    # 监听队列
+    #s.listen(5)
+    
+    def _server_print(*msg):
+        print("SERVER: %s" % msg)
+    
+    def _clean_up():
+        if os.path.exists(host.logfile):
+            os.remove(host.logfile)
+    
+    _clean_up()
+    
+    # 简单地监听客户端的消息，收到消息后回复确认
+    while True:
+        _server_print("=== THE SERVER START WORKING ===")
+       # 此处的s.recvfrom方法和s.recv都会返回接收到的数据，而recvfrom还会返回客户端的地址
+        data, addr = s.recvfrom(host.BUFFERSIZE)
+        data = data.decode(encoding='utf-8')
+        _server_print("recived message from client(%s): %s" % (addr, data))
+        with open(host.logfile, 'a') as f:
+            log = "client{0} \t message: {1}\n".format(addr, data)
+            f.write(log)
+    
+       
+       # 回复消息
+        s.sendto("SERVER ROGER THAT".encode(encoding='utf-8'), addr)
+    
+    if not s._closed():
+        s.close()
 
-# socket实例，TCP连接 
-s = socket(AF_INET, SOCK_DGRAM)
-# 重用ip和端口
-s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-# s绑定主机地址和端口
-s.bind((HOST, PORT))
-# 监听队列
-#s.listen()
-# 服务器对话日志
-logfile = 'dialog.log'
-
-def server_print(*msg):
-    print("SERVER: ", msg)
-
-def clean_up():
-    if os.path.exists(logfile):
-        os.remove(logfile)
-
-clean_up()
-
-# 简单地监听客户端的消息，收到消息后回复确认
-while True:
-    print("=== THE SERVER START WORKING ===")
-   # 此处的s.recvfrom方法和s.recv都会返回接收到的数据，而recvfrom还会返回客户端的地址
-    data, addr = s.recvfrom(BUFFERSIZE)
-    data = data.decode(encoding='utf-8')
-    server_print("recived message from client(%s): %s" % (addr, data))
-    with open(logfile, 'a') as f:
-        log = "client{0} \t message: {1}\n".format(addr, data)
-        f.write(log)
-
-   
-   # 回复消息
-    s.sendto("SERVER ROGER THAT ".encode(encoding='utf-8'), addr)
-
-if not s._closed():
-    s.close()
-
+if __name__ == '__main__':
+    start_udp_server()
